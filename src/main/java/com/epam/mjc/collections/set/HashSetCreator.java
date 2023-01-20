4cc299fe136f8c3496bcadfa5e67c551f5c899d4
MartaKrawczy <martaakrawczykowska@gmail.com>
2023-01-20 19:38:27 +0100

    initial commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,22 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer>create = new HashSet<Integer>(sourceList);
+        int sum;
+        for (int i = 0 ; i<sourceList.size(); i++) {
+           if( sourceList.get(i)%2 == 0){
+               create.add(sourceList.get(i));
+               sum = sourceList.get(i);
+               while (sum % 2 == 0) {
+                  sum = sum /2 ;
+                   create.add(sum);
+               }
+           }
+           else {
+               create.add(sourceList.get(i));
+               create.add(sourceList.get(i)*2);
+           }
+        }
+        return create;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,19 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
-    public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+
+        public Set<Integer> createSubsetOfSquares (List < Integer > sourceList,int lowerBound, int upperBound){
+           Set<Integer> setlist = new TreeSet<>();
+
+            for (int i=0; i<sourceList.size(); i++) {
+                if ((Math.pow(sourceList.get(i),2)>=lowerBound) && (Math.pow(sourceList.get(i),2)<=upperBound) ){
+                    setlist.add((int) Math.pow(sourceList.get(i),2));
+                }
+            }
+            return setlist;
+        }
     }
-}
+
