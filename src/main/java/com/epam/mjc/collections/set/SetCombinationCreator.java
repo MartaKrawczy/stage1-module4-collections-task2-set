b08c874003aa94e30faa392832bf70ba4aec6679
MartaKrawczy <martaakrawczykowska@gmail.com>
2023-01-24 15:50:46 +0100

    add 3 task

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,10 +1,26 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-        return null;
+
+        Set<String> collect = firstSet.stream().filter(secondSet::contains).collect(Collectors.toSet());
+
+        Set<String> collect1 = thirdSet.stream().filter(x -> !firstSet.contains(x)).filter(x -> !secondSet.contains(x)).collect(Collectors.toSet());
+        for (String s : collect1) {
+            if(collect.contains(s)){
+                collect1.remove(s);
+               collect.remove(s);
+            }else {
+                collect.add(s);
+            }
+        }
+        return collect;
     }
+
+
+
 }
+
